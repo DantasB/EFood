14d1b5b5bbde8ea70a57617818e75c6be03aef5a
Bruno Dantas <bruno.d.paiva@hotmail.com>
2022-12-08 09:09:33 -0300

    Fix delegate

--- efood/src/main/java/com/xpto/efood/delegate/ThirdParty.java
+++ efood/src/main/java/com/xpto/efood/delegate/ThirdParty.java
@@ -3,7 +3,6 @@ package com.xpto.efood.delegate;
 import com.xpto.efood.service.dto.PedirProcessDTO;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,13 +10,10 @@ public class ThirdParty implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        PedirProcessDTO orderProcess = (PedirProcessDTO) delegateExecution.getVariable("pi");
-        
-        //Confirming the flight
         System.out.println("OrderService: ###########################################");
         System.out.println("OrderService: ###########################################");
         System.out.println("OrderService: ###########################################");
-        System.out.println("OrderService:        Pedido " +  orderProcess.getPedir().getIdPedido()) + " aceito!");
+        System.out.println("OrderService:        Pedido " +  delegateExecution.getVariable("processInstance") + " aceito!");
         System.out.println("OrderService: ###########################################");
         System.out.println("OrderService: ###########################################");
         System.out.println("OrderService: ###########################################\n\n\n");
